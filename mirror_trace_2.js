1819b63e2582bc46f9d0f2f946b93a7323c77da2
onyulhaq <92550196+onyulhaq@users.noreply.github.com>
2024-01-25 00:55:55 -0800

    audio uploaded

--- mirror_trace_2.js
+++ mirror_trace_2.js
@@ -69,7 +69,10 @@ var saveTrace = false;
 //image dimensions
 var mywidth = 400;
 var myheight = 300;
-
+var audio = new Audio(
+  "https://raw.githubusercontent.com/onyulhaq/mirror_trace/master/audio_file.mp3"
+);
+audio.play();
 var score = 0;
 var timeDiff = 0;
 var trialnumber = 0;
@@ -108,7 +111,7 @@ function do_mirror() {
   var endRadius = 7;
 
   //states to track
-  drawing = true; //////////////////////////// Need to change back to false when done
+  drawing = false; //////////////////////////// Need to change back to false when done
   finished = false;
 
   score = 0;
@@ -301,11 +304,6 @@ function do_mirror() {
             crossings = crossings + 1;
             distance_inline = distance_inline + 0.5 * distance_current;
             distance_offline = distance_offline + 0.5 * distance_current;
-            console.log({
-              distance_inline: distance_inline,
-              distance_offline: distance_offline,
-            });
-
             ctx_mirror.beginPath();
             if (mirror) {
               ctx_mirror.moveTo(mywidth - mouse.x, myheight - mouse.y);
@@ -313,11 +311,49 @@ function do_mirror() {
               ctx_mirror.moveTo(mouse.x, mouse.y);
             }
           }
+        } else {
+          if (inline) {
+            inline = false;
+
+            crossings = crossings + 1;
+            distance_inline = distance_inline + 0.5 * distance_current;
+            distance_offline = distance_offline + 0.5 * distance_current;
+            ctx_mirror.beginPath();
+            if (mirror) {
+              ctx_mirror.moveTo(mywidth - mouse.x, myheight - mouse.y);
+            } else {
+              ctx_mirror.moveTo(mouse.x, mouse.y);
+            }
+          } else {
+            distance_offline = distance_offline + distance_current;
+          }
         }
+
+        // distance_total how far we are from the top left corner summed up
+        //distance inline -
+        //score - proportion of the mousemovement spent on tracing the image
+        // distance inline = cursor.distance
         distance_total = distance_total + distance_current;
         score = distance_inline / distance_total;
         endTime = new Date();
         timeDiff = (endTime - startTime) / 1000;
+
+        if (inline) {
+          ctx_mirror.strokeStyle = "red";
+        } else {
+          ctx_mirror.strokeStyle = "blue";
+          alert("You are off");
+        }
+
+        if (mirror) {
+          ctx_mirror.lineTo(mywidth - mouse.x, myheight - mouse.y);
+        } else {
+          ctx_mirror.lineTo(mouse.x, mouse.y);
+        }
+        ctx_mirror.stroke();
+        document.getElementById("status").innerHTML =
+          "Score = " + Math.round(score * 100) + "% ";
+        //document.getElementByID("status").innerHTML = p[0]+p[1]+p[2];
       }
 
       // console.log({
@@ -335,20 +371,174 @@ function do_mirror() {
       // "p[1]": p[1],
       // "p[2]": p[2],
       // });
+      else {
+        if (!finished) {
+          currentRefresh = new Date();
+          if (currentRefresh - lastRefresh > 1000 / 30) {
+            ctx_mirror.drawImage(imageObj, 0, 0, mywidth, myheight);
+
+            ctx_mirror.fillStyle = "green";
+            ctx_mirror.globalAlpha = 0.4;
+            //ctx_mirror.beginPath();
+            if (mirror) {
+              //	ctx_mirror.arc(mywidth - xstart, myheight - ystart, startRadius, 0, 2 * Math.PI, false);
+            } else {
+              //	ctx_mirror.arc(xstart, ystart, startRadius, 0, 2 * Math.PI, false);
+            }
+            // ctx_mirror.fill();
+            ctx_mirror.globalAlpha = 1;
+
+            ctx_mirror.beginPath();
+            if (mirror) {
+              ctx_mirror.arc(
+                mywidth - mouse.x,
+                myheight - mouse.y,
+                4,
+                0,
+                2 * Math.PI,
+                false
+              );
+            } else {
+              ctx_mirror.arc(mouse.x, mouse.y, 4, 0, 2 * Math.PI, false);
+            }
+            ctx_mirror.fillStyle = "green";
+            ctx_mirror.fill();
+            lastRefresh = currentRefresh;
+            document.getElementById("status").innerHTML =
+              "Click the green circle to begin this trial";
+          }
+        } else {
+          document.getElementById("status").innerHTML =
+            "Finished with score = " +
+            Math.round(score * 100) +
+            "%<BR> Click next to continue.";
+        }
+      }
+
+      //store current coordinates
+      mouseold.x = mouse.x;
+      mouseold.y = mouse.y;
     },
-    console.log(ctx_mirror)
+    false
   );
-}
+  canvas.addEventListener(
+    "mousedown",
+    function (e) {
+      var currentRadius = Math.sqrt(
+        Math.pow(mouse.x - xstart, 2) + Math.pow(mouse.y - ystart, 2)
+      );
 
-function betterPos(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top,
+      if (!finished) {
+        if (drawing) {
+          //drawing = false;
+          //finished = true;
+          //if (saveTrace) {
+          //	saveCanvas();
+          //call save function
+          //savecanvas(canvas_mirror.toDataURL())
+          //}
+        } else {
+          if (currentRadius < startRadius) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx_mirror.drawImage(imageObj, 0, 0, mywidth, myheight);
+            ctx_mirror.fillStyle = "red";
+            ctx_mirror.globalAlpha = 0.4;
+            ctx_mirror.beginPath();
+            if (mirror) {
+              ctx_mirror.arc(
+                mywidth - xend,
+                myheight - yend,
+                endRadius,
+                0,
+                2 * Math.PI,
+                false
+              );
+            } else {
+              ctx_mirror.arc(xend, yend, endRadius, 0, 2 * Math.PI, false);
+            }
+            ctx_mirror.fill();
+            ctx_mirror.globalAlpha = 1;
+
+            drawing = true;
+            finished = false;
+            startTime = new Date();
+            ctx_mirror.beginPath();
+            if (mirror) {
+              ctx_mirror.moveTo(mywidth - mouse.x, myheight - mouse.y);
+            } else {
+              ctx_mirror.moveTo(mouse.x, mouse.y);
+            }
+          }
+        }
+      }
+    },
+    false
+  );
+
+  var onPaint = function () {
+    if (mirror) {
+      ctx_mirror.lineTo(mywidth - mouse.x, myheight - mouse.y);
+    } else {
+      ctx_mirror.lineTo(mouse.x, mouse.y);
+    }
+    ctx_mirror.stroke();
   };
-}
 
-function rgbToHex(r, g, b) {
-  if (r > 255 || g > 255 || b > 255) throw "Invalid color component";
-  return ((r << 16) | (g << 8) | b).toString(16);
+  function betterPos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top,
+    };
+  }
+  function findPos(obj) {
+    var curleft = 0,
+      curtop = 0;
+    //document.getElementById("status").innerHTML = "Find pos: ";
+
+    if (obj.offsetParent) {
+      do {
+        curleft += obj.offsetLeft;
+        curtop += obj.offsetTop;
+        document.getElementById("status").innerHTML +=
+          obj.id + " Left: " + obj.offsetLeft + "Top: " + obj.offsetTop + " / ";
+      } while ((obj = obj.offsetParent));
+      return { x: curleft, y: curtop };
+    }
+    return undefined;
+  }
+
+  function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255) throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+  }
+  function saveCanvas() {
+    // Get the canvas screenshot as PNG
+    var screenshot = Canvas2Image.saveAsPNG(canvas_mirror, true);
+
+    // This is a little trick to get the SRC attribute from the generated <img> screenshot
+    canvas_mirror.parentNode.appendChild(screenshot);
+    screenshot.id = "canvasimage";
+    data = screenshot.src;
+    canvas_mirror.parentNode.removeChild(screenshot);
+
+    // Send the screenshot to PHP to save it on the server
+    var url = saveScript;
+
+    jQuery.ajax({
+      type: "POST",
+      url: url,
+      dataType: "text",
+      data: {
+        id: MID,
+        trial: trialnumber,
+        score: score,
+        distance_inline: distance_inline,
+        distance_offline: distance_offline,
+        timeDiff: timeDiff,
+        crossings: crossings,
+        base64data: data,
+      },
+    });
+  }
 }
